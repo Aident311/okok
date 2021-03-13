982525317159e667aa606cbea799dddcf8b9ce1e
Glitch (chill-pepper-shade) <none>
2021-03-13 22:55:15 +0000

    🏎🍴 Checkpoint ./welcome.js:29643565/465

--- welcome.js
+++ welcome.js
@@ -1,1 +1,16 @@
-re
+module.exports = (client) => {
+  const channelId = "810893317311037450";
+  const rulesChannel = "794083029558755348";
+  client.on("guildMemberAdd", (member) => {
+    console.log(member);
+
+    const message = `Welcome <@${
+      member.id
+    }> to TechnoCraft!! Be sure to check out our ${member.guild.channels.cache
+      .get(rulesChannel)
+      .toString()}`;
+
+    const channel = member.guild.channels.cache.get(channelId);
+    channel.send(message);
+  });
+};
